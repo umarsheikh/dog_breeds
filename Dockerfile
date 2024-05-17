284d4338538ff5aa0c1005ed6b06b2224389ddc8
umar <umar.m.sheikh@gmail.com>
2024-05-17 18:55:29 +0500

    Do minor cleanup and make specs work

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,23 @@
-# Use the official Ruby image
 FROM ruby:3.3.1
 
-# Set the working directory
 WORKDIR /app
 
-# Install dependencies
 RUN apt-get update -qq && apt-get install -y \
     build-essential \
     libsqlite3-dev \
     nodejs \
     npm
 
-# Install yarn for managing JavaScript packages
 RUN npm install -g yarn
 
-# Copy the Gemfile and Gemfile.lock
 COPY Gemfile Gemfile.lock ./
 
-# Install the required Ruby gems
 RUN bundle install
 
-# Copy the rest of the application code
 COPY . .
 
-# Precompile assets for production
-# RUN bundle exec rake assets:precompile
-
-# Expose port 3000 to the host
 EXPOSE 3000
 
-# Set the entrypoint
 ENTRYPOINT ["./entrypoint.sh"]
 
-# Start the Rails server
 CMD ["rails", "server", "-b", "0.0.0.0"]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'webmock/rspec'
+require 'selenium/webdriver'
+require 'capybara/rspec'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -10,20 +12,6 @@ require 'rspec/rails'
 
 Capybara.javascript_driver = :selenium_chrome_headless
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -43,19 +31,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
   #
   # The different available types are documented in the features, such as in
   # https://rspec.info/features/6-0/rspec-rails
